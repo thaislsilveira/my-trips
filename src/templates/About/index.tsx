4980e749d0477954e5c94668e6f30b0665d392e7
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-11 09:27:01 -0300

    feat: add About

---
+++ src/templates/About/index.tsx
@@ -0,0 +1,25 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} />
+    </LinkWrapper>
+
+    <S.Heading>My Trips</S.Heading>
+
+    <S.Body>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis
+        enim quis condimentum venenatis. In ultricies rhoncus purus, at
+        imperdiet sem luctus sit amet. Fusce tristique aliquet libero, eget
+        mattis dui hendrerit non.
+      </p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
