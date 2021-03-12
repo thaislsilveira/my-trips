afcd4c69c9c6c7f3220866048fdd0ac0f907ff8a
Thaís Silveira <thais_lsilveira@hotmail.com>
2021-03-12 09:35:01 -0300

    feat:  types generation to Graphql

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,6 +3,7 @@ import { GET_PAGES, GET_PAGE_BY_SLUG } from '../graphql/queries'
 import PagesTemplate, { PageTemplateProps } from 'templates/Pages'
 import { useRouter } from 'next/dist/client/router'
 import { GetStaticProps } from 'next'
+import { GetPageBySlugQuery, GetPagesQuery } from 'grapqhl/generated/graphql'
 
 export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
@@ -13,7 +14,7 @@ export default function Page({ heading, body }: PageTemplateProps) {
 }
 
 export async function getStaticPaths() {
-  const { pages } = await client.request(GET_PAGES, { firat: 3 })
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
 
   const paths = pages.map(({ slug }) => ({
     params: { slug }
@@ -23,7 +24,7 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
     slug: `${params?.slug}`
   })
 
